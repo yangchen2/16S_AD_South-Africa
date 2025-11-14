512f5c5dc1e6396da97803b379c99c86f34a6a39
yangchen2 <yangchen4379@gmail.com>
2025-11-13 17:04:47 -0800

    update biom to tsv script

--- Scripts/10_biom_to_tsv.sh
+++ Scripts/10_biom_to_tsv.sh
@@ -1,13 +1,13 @@
 # Step 1: Convert HDF5 → JSON BIOM
 biom convert \
-  -i ../Data/Tables/Count_Tables/6_3_209766_feature_table_dedup_prev-filt-10pct_Genus-ASV_all.biom \
-  -o ../Data/Tables/Count_Tables/6_3_209766_feature_table_dedup_prev-filt-10pct_Genus-ASV_all_json.biom \
+  -i ../Data/Tables/Count_Tables/6_209766_feature_table_dedup_prev-filt-10pct_Genus-ASV_all.biom \
+  -o ../Data/Tables/Count_Tables/6_209766_feature_table_dedup_prev-filt-10pct_Genus-ASV_all_json.biom \
   --to-json
 
 # Step 2: Convert JSON BIOM → TSV
 biom convert \
-  -i ../Data/Tables/Count_Tables/6_3_209766_feature_table_dedup_prev-filt-10pct_Genus-ASV_all_json.biom \
-  -o ../Data/Tables/Count_Tables/6_3_209766_feature_table_dedup_prev-filt-10pct_Genus-ASV_all.tsv \
+  -i ../Data/Tables/Count_Tables/6_209766_feature_table_dedup_prev-filt-10pct_Genus-ASV_all_json.biom \
+  -o ../Data/Tables/Count_Tables/6_209766_feature_table_dedup_prev-filt-10pct_Genus-ASV_all.tsv \
   --to-tsv
 
-# You will then need to get rid of the header row and the index name (start with #)
+# You will then need to get rid of the header row and the index name (start with #) before you run the plotting notebooks
